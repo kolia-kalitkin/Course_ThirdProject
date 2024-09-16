d6662774fd2c6e94a6d09a71ad6982378eb7613c
Андрей Седельников <nikolay.kalitkin@gmail.com>
2024-09-16 20:37:01 +0600

    создание  формы на сранице

--- crud/views.py
+++ crud/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
-
-# Create your views here.
+from .forms import UserForm
+ 
+def index(request):
+    userform = UserForm()
+    return render(request, 'index.html', {'form': userform})
