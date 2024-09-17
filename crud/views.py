a26b87d46af88e1299537069771900ee0e290368
Андрей Седельников <nikolay.kalitkin@gmail.com>
2024-09-17 23:21:18 +0600

    отображение поля и связанных с ним атрибутов - ошибок, текста подсказки, метки и т.д

--- crud/views.py
+++ crud/views.py
@@ -4,13 +4,10 @@ from .forms import UserForm
 
 
 def index(request):
+    userform = UserForm()
     if request.method == 'POST':
-        userform = UserForm(request.POST)   # надо создать объект формы и передать форме пришедшие из запроса данные.
-        if userform.is_valid():             # проверяем их корректность
+        userform = UserForm(request.POST)
+        if userform.is_valid():
             name = userform.cleaned_data['name']
             return HttpResponse(f'<h2>Hello, {name}</h2>')
-        else:
-            return HttpResponse('Invalid data')
-    else:
-        userform = UserForm()
-        return render(request, 'index.html', {'form': userform})
+    return render(request, 'index.html', {"form": userform})
