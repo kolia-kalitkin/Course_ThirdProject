c44402f7440cbe28b7bd60e283aea869e5b276f1
Андрей Седельников <nikolay.kalitkin@gmail.com>
2024-09-17 21:40:54 +0600

    проверка данных на валидность

--- crud/forms.py
+++ crud/forms.py
@@ -2,7 +2,6 @@ from django import forms
 
 
 class UserForm(forms.Form):
-    name = forms.CharField(label='Имя', initial='undefined', help_text='Введите свое имя')
-    age = forms.IntegerField(label='Ваш возраст?', initial=18, help_text='Введите свой возраст')
-    
-    field_order = ['age', 'name']   # порядок полей на форме
+    name = forms.CharField(label='Имя', help_text='Введите свое имя', min_length=2, max_length=10)
+    age = forms.IntegerField(label='Ваш возраст?', help_text='Введите свой возраст')
+    reklama = forms.BooleanField(label='Coглacны получать рекламу?', required=False)
--- crud/views.py
+++ crud/views.py
@@ -5,9 +5,12 @@ from .forms import UserForm
 
 def index(request):
     if request.method == 'POST':
-        name = request.POST.get('name') # получить значение поля Имя
-        age = request.POST.get('age') # получить значение поля Возраст
-        return HttpResponse(f'<h2>Привет, {name}, твой возраст: {age}</h2>')
+        userform = UserForm(request.POST)   # надо создать объект формы и передать форме пришедшие из запроса данные.
+        if userform.is_valid():             # проверяем их корректность
+            name = userform.cleaned_data['name']
+            return HttpResponse(f'<h2>Hello, {name}</h2>')
+        else:
+            return HttpResponse('Invalid data')
     else:
         userform = UserForm()
         return render(request, 'index.html', {'form': userform})
