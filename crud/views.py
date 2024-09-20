5f4deb2e734491c08172ba8e09bf18afb7547af9
Андрей Седельников <nikolay.kalitkin@gmail.com>
2024-09-20 23:08:03 +0600

    данные о клиентах, которые мы занесли в БД с помощью приложения crud

--- crud/views.py
+++ crud/views.py
@@ -1,13 +1,19 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-from .forms import UserForm
+from django.http import HttpResponseRedirect, HttpResponseNotFound
+from .models import Person
 
 
+# Получение данных из БД
 def index(request):
-    userform = UserForm()
+    people = Person.objects.all()
+    return render(request, 'index.html', {'people': people})
+
+
+# Сохранение данных в БД
+def create(request):
     if request.method == 'POST':
-        userform = UserForm(request.POST)
-        if userform.is_valid():
-            name = userform.cleaned_data['name']
-            return HttpResponse(f'<h2>Hello, {name}</h2>')
-    return render(request, 'index.html', {"form": userform})
+        person = Person()
+        person.name = request.POST.get('name')
+        person.age = request.POST.get('age')
+        person.save()
+    return HttpResponseRedirect('/')
