8ccfe986525712efd593f7eda00f8fb8aa7cd1ca
Андрей Седельников <nikolay.kalitkin@gmail.com>
2024-09-21 00:20:58 +0600

    добавил возможность редактирования и удаления объектов модели

--- crud/views.py
+++ crud/views.py
@@ -17,3 +17,28 @@ def create(request):
         person.age = request.POST.get('age')
         person.save()
     return HttpResponseRedirect('/')
+
+
+# Изменение данных в БД
+def edit(request, id):
+    try:
+        person = Person.objects.get(id=id)
+        if request.method == 'POST':
+            person.name = request.POST.get('name')
+            person.age = request.POST.get('age')
+            person.save()
+            return HttpResponseRedirect('/')
+        else:
+            return render(request, 'edit.html', {'person': person})
+    except Person.DoesNotExist:
+        return HttpResponseNotFound('<h2>Person not found</h2>')
+
+
+# Удаление данных из БД
+def delete(request, id):
+    try:
+        person = Person.objects.get(id=id)
+        person.delete()
+        return HttpResponseRedirect('/')
+    except Person.DoesNotExist:
+        return HttpResponseNotFound('<h2>Person not found</h2>')
